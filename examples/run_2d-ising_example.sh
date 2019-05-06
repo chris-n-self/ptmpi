3158fd22b1b9bcf0b98df22a6d1edf3b726d4d9d
chris-n-self <chris.self@btinternet.com>
2019-05-06 17:13:26 +0100

    tweaks to examples/run_2d-ising_example.sh

--- examples/run_2d-ising_example.sh
+++ examples/run_2d-ising_example.sh
@@ -1,9 +1,8 @@
 
 # cleanup from any previous runs
 rm -r *.txt
-cd output 
-rm -r *.json
-cd ..
+if [ -d output ]; then rm -r output; fi
+mkdir output
 
 # run mpiexec with 20 processes, executing python script '2d-ising.py'
 mpiexec -n 20 python 2d-ising.py
